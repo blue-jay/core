7009ea105ba88a878f1b4789364d3d9b859c1b1b
Joseph Spurrier <github@josephspurrier.com>
2016-11-13 20:46:33 -0500

    Update a test to file

--- storage/migration/mysql/mysql_test.go
+++ storage/migration/mysql/mysql_test.go
@@ -181,7 +181,7 @@ func TestInsertRows(t *testing.T) {
 	}
 
 	// Test querying the data
-	result, _ := con.byID("1")
+	result, _ := con.byID("2")
 	if result.Name != "Joey" {
 		t.Errorf("record retrieved is incorrect: '%v'", result.Name)
 	}
